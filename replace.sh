92dcbe4df357d816520877fbcf160dcf3e87dc62
Giuseppe Raiolo <104866472+graiolo@users.noreply.github.com>
2023-02-22 22:48:14 +0100

    fix output

--- replace.sh
+++ replace.sh
@@ -6,7 +6,7 @@ replacement="$(pwd)"
 for file in "$@"; do
     while (true); do
         if grep -q "$regex" "$file"; then
-            awk -v regex="$regex" -v replacement="$replacement" '{gsub(regex, replacement)}' "$file" > tmpfile && mv tmpfile "$file"
+            awk -v regex="$regex" -v replacement="$replacement" '{gsub(regex, replacement)}1' "$file" > tmpfile && mv tmpfile "$file"
         else
             break
         fi

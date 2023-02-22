4bc9f2a7d0649207e15bba08fea9cf80014b2ef4
Giuseppe Raiolo <104866472+graiolo@users.noreply.github.com>
2023-02-22 22:44:14 +0100

    Update replace.sh

--- replace.sh
+++ replace.sh
@@ -7,9 +7,7 @@ for file in "$@"; do
     while (true); do
         if grep -q "$regex" "$file"; then
             awk -v regex="$regex" -v replacement="$replacement" '{gsub(regex, replacement)}' "$file" > tmpfile && mv tmpfile "$file"
-            echo "Sostituita la parola '$regex' con '$replacement' nel file $file"
         else
-            echo "La parola '$regex' non è presente nel file $file"
             break
         fi
     done

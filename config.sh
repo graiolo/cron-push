4e7c97ec58ee004e82e68479b78d026c390f44a0
Giuseppe Raiolo <104866472+graiolo@users.noreply.github.com>
2023-03-09 15:03:30 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,9 +1,7 @@
 chmod +x off-push.sh crono-push.sh
 
-./replace.sh crono-push.sh.desktop
-./replace.sh off-push.sh.desktop
-./replace.sh crono-push.sh
-./replace.sh off-push.sh
+./replace.sh crono-push.sh.desktop off-push.sh.desktop
+./replace.sh crono-push.sh off-push.sh
 
 if [ ! -d ~/.config ]
 	then
@@ -15,13 +13,8 @@ if [ ! -d ~/.config/autostart ]
 		mkdir ~/.config/autostart
 fi
 
-rm -f ~/.config/autostart/crono-push.sh.desktop
-rm -f ~/.config/autostart/off-push.sh.desktop
+rm -f ~/.config/autostart/crono-push.sh.desktop off-push.sh.desktop
 
-mv crono-push.sh.desktop ~/.config/autostart/
-mv off-push.sh.desktop ~/.config/autostart/
+mv crono-push.sh.desktop off-push.sh.desktop ~/.config/autostart/
 
-rm -rf .git
-rm -f README.md
-rm -f replace.sh
-rm -f config.sh
+rm -rf .git README.md replace.sh config.sh
